c71aaec9ac83af79b71266b6fe1fb2d2cd5bfbe3
luyueyin <12898@coderacademy.edu.au>
2023-02-07 20:13:35 +1100

    React comps: Use external API

--- my-first-react-app/src/components/App.js
+++ my-first-react-app/src/components/App.js
@@ -2,12 +2,16 @@ import { useState } from "react";
 // importing default export - name can be different
 // import Product from "./Product";
 // importing named export - name must be same
-import { ProductList } from "./ProductList";
+
+
+// import { ProductList } from "./ProductList";
 import Cart from "./Cart";
 import NavBar from "./mui/NavBar";
 
 import { CircularProgress } from "@mui/material";
 import Box from "@mui/material/Box";
+
+import ProductListClass from "./ProductListClass";
 
 const App = () => {
   const [isLoading, setIsloading] = useState(true);
@@ -31,7 +35,7 @@ const App = () => {
         // jsx
         <div className="App">
           <NavBar />
-          <ProductList />
+          <ProductListClass />
           <Cart />
         </div>
       )}
